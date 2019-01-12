242308c9d89575b41429fc2bbea8d92b36621fd2
lucianprunau <40663290+lucianprunau@users.noreply.github.com>
2019-01-12 17:27:56 +0200

    Initial

---
+++ billing/src/test/java/com/aos/onlineshop/billing/BillingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.aos.onlineshop.billing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
